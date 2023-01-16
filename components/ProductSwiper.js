3c3153305ad63ff949678b304e1934bce361e79e
kamilstalinski <kamstal95@gmail.com>
2023-01-16 13:34:20 +0100

    fixed routing in products page

--- components/ProductSwiper.js
+++ components/ProductSwiper.js
@@ -8,6 +8,7 @@ import products from "../util/products.json";
 const ProductSwiper = ({ t }) => {
   const router = useRouter();
   const productObj = router.locale === "pl" ? products.pl : products.en;
+  console.log(router);
 
   return (
     <>
@@ -23,7 +24,7 @@ const ProductSwiper = ({ t }) => {
         className='product-swiper'>
         {productObj.map((product) => (
           <SwiperSlide key={product.id} style={{ maxWidth: "310px" }}>
-            <Link href={`products/${product.path}`}>
+            <Link href={product.path}>
               <a className='link-container'>
                 <Image
                   width={300}
