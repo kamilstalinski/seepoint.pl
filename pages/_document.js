0483065140b21ab472780507aabf4b2417204f39
kamilstalinski <kamstal95@gmail.com>
2023-02-02 13:57:17 +0100

    added LocalBusiness, OpenGraph and Twitter microformats

--- pages/_document.js
+++ pages/_document.js
@@ -1,43 +1,50 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { DefaultSeo, LocalBusinessJsonLd } from "next-seo";
 
 export default function Document() {
   return (
     <Html>
       <Head>
-        <meta name='robots' content='noindex, nofollow' />
-        <meta
-          name='description'
-          content='Oferujemy: druk sublimacyjny, solwentowy, uv oraz plotery tnące. Wybierając Seepoint Sp. z o.o. decydujecie się Państwo na współpracę z profesjonalistami.'
+        <DefaultSeo
+          openGraph={{
+            type: "website",
+            locale: "pl_PL",
+            localeAlternate: "en_GB",
+            title: "Seepoint: PRODUCENT REKLAMY WIZUALNEJ",
+            description:
+              "Oferujemy: druk sublimacyjny, solwentowy, uv oraz plotery tnące. Wybierając Seepoint Sp. z o.o. decydujecie się Państwo na współpracę z profesjonalistami.",
+            url: "https://seepoint.pl/",
+            siteName: "Seepoint",
+            images: [
+              {
+                url: "https://seepoint.pl/app/uploads/2019/06/LOGOFoot.png",
+                width: 345,
+                height: 126,
+                alt: "Seepoint Logo",
+                type: "image/png",
+              },
+            ],
+          }}
+          twitter={{
+            cardType: "summary_large_image",
+          }}
         />
-        <meta charSet='UTF-8' />
-        <meta property='og:locale' content='pl_PL' />
-        <meta property='og:locale:alternate' content='en_EN' />
-        <meta property='og:type' content='website' />
-        <meta
-          property='og:title'
-          content='Seepoint: PRODUCENT REKLAMY WIZUALNEJ'
-        />
-        <meta
-          property='og:description'
-          content='Oferujemy: druk sublimacyjny, solwentowy, uv oraz plotery tnące. Wybierając Seepoint Sp. z o.o. decydujecie się Państwo na współpracę z profesjonalistami.'
-        />
-        <meta property='og:url' content='https://seepoint.pl/' />
-        <meta property='og:site_name' content='Seepoint' />
-        <meta
-          property='article:modified_time'
-          content='2022-06-06T09:09:33+00:00'
-        />
-        <meta
-          property='og:image'
-          content='https://seepoint.pl/app/uploads/2019/06/LOGOFoot.png'
-        />
-        <meta property='og:image:width' content='345' />
-        <meta property='og:image:height' content='126' />
-        <meta property='og:image:type' content='image/png' />
-        <meta name='twitter:card' content='summary_large_image' />
-        <meta
-          name='google-site-verification'
-          content='HyyuNRLZYF3X4Rw04vbdOaA_OYyVMK8Pkizldz5_b9E'
+        <LocalBusinessJsonLd
+          name='Seepoint'
+          images={[
+            "https://seepoint.pl/app/themes/seepoint-theme/assets/img/logo_seepoint@2x.png",
+          ]}
+          telephone={["+48914181191", "+48667114119"]}
+          address={{
+            streetAdress: "ul. I Brygady Legionów 15",
+            adressLocality: "Goleniów",
+            postalCode: "72-100",
+            adressCountry: "PL",
+          }}
+          geo={{
+            latitude: "53.5574121",
+            longitude: "14.8476345",
+          }}
         />
       </Head>
       <body>
