98f551e159d96d46aee751c849c4ee563ac2d020
kamilstalinski <kamstal95@gmail.com>
2023-02-02 21:03:45 +0100

    fixed compile error

--- components/ShopButton.js
+++ components/ShopButton.js
@@ -1,7 +1,7 @@
 export default function ShopButton({ FontAwesomeIcon, faCartShopping }) {
   return (
     <button className='shop-btn'>
-      <a href='https://sklep.seepoint.pl/' target='_blank'>
+      <a href='https://sklep.seepoint.pl/' target='_blank' rel='noreferrer'>
         <FontAwesomeIcon icon={faCartShopping} />
         <p>Sklep</p>
       </a>
